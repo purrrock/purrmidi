04d63dac2dffbb3d55530a304dbe7c0843f72998
Litvin Vasil <purrrock@gmail.com>
2026-09-19 17:14:29 +0300

    Clock change

--- Core/Src/i2s.c
+++ Core/Src/i2s.c
@@ -71,7 +71,7 @@ void HAL_I2S_MspInit(I2S_HandleTypeDef* i2sHandle)
   /** Initializes the peripherals clock
   */
     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_I2S;
-    PeriphClkInitStruct.PLLI2S.PLLI2SN = 384;
+    PeriphClkInitStruct.PLLI2S.PLLI2SN = 64;
     PeriphClkInitStruct.PLLI2S.PLLI2SM = 16;
     PeriphClkInitStruct.PLLI2S.PLLI2SR = 5;
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
